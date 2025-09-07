db8f7a4d95bd246b507fdc038617a63eb11c8333
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-09-07 14:41:37 +0330

    Create 2025_09_07_070100_create_cases_table.php

---
+++ database/migrations/2025_09_07_070100_create_cases_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cases', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->nullable();
+            $table->string('contract_number')->index();
+            $table->string('application_type')->nullable();
+            $table->string('status')->default('in_progress');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('application_cases');
+    }
+};
+
+
