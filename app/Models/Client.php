1cf1de6c4443fc1851aaa8d755a20ef07f50dd4d
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-09-07 14:41:26 +0330

    Create Client.php

---
+++ app/Models/Client.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function cases(): HasMany
+    {
+        return $this->hasMany(ClientCase::class, 'client_id');
+    }
+}
+
+
