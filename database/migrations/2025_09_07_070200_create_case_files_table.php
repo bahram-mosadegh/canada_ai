fe65782516b52595a551bdaa6d40df47552f0143
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-09-07 14:41:41 +0330

    Create 2025_09_07_070200_create_case_files_table.php

---
+++ database/migrations/2025_09_07_070200_create_case_files_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('case_files', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('case_id')->nullable();
+            $table->string('original_name')->nullable();
+            $table->string('mime_type')->nullable();
+            $table->string('path')->nullable();
+            $table->string('type')->nullable();
+            $table->string('ai_model')->nullable();
+            $table->string('ai_detected_type')->nullable();
+            $table->json('ai_data')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('case_files');
+    }
+};
+
+
