2a886fc2d3f3a8f411eb1f0fd51b1f996b34a96c
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-09-07 14:41:15 +0330

    Create CaseIntakeController.php

---
+++ app/Http/Controllers/CaseIntakeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CaseIntakeController extends Controller
+{
+    public function show()
+    {
+        return view('intake');
+    }
+}
+
+
