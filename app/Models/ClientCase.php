fc452ac61f1de359323e26cf55586380b7da9193
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-09-07 14:41:29 +0330

    Create ClientCase.php

---
+++ app/Models/ClientCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class ClientCase extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cases';
+
+    protected $guarded = [];
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function files(): HasMany
+    {
+        return $this->hasMany(CaseFile::class, 'case_id');
+    }
+}
+
+
