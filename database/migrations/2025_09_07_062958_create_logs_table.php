8e9340c1dacdf2305607ab79d546b4101ddad1c2
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-09-07 13:17:04 +0330

    Create 2025_09_07_062958_create_logs_table.php

---
+++ database/migrations/2025_09_07_062958_create_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('loggable');
+            $table->foreignId('user_id')->nullable();
+            $table->enum('action', ['add', 'remove', 'edit', 'success', 'fail', 'other']);
+            $table->json('data')->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logs');
+    }
+};
