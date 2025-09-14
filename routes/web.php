c3362b50de2bb76c82c3ba9b0af6b31231edbe23
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-09-14 13:30:47 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\CaseIntakeController;
+use App\Http\Controllers\DesktopController;
 use App\Http\Controllers\InternalApiController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\CaseController;
 
 Route::group(['middleware' => 'auth'], function () {
 
     Route::get('/', function () {
         return redirect('/desktop');
+    });
+
+    Route::controller(ClientController::class)->group(function () {
+        Route::get('clients', 'index');
+        // Route::get('add_user', 'add_get');
+        // Route::post('add_user', 'add_post');
+        // Route::get('edit_user/{id}', 'edit_get');
+        // Route::post('edit_user/{id}', 'edit_post');
+    });
+
+    Route::controller(CaseController::class)->group(function () {
+        Route::get('cases', 'index');
     });
 
     Route::controller(UserController::class)->group(function () {
@@ -23,7 +37,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::post('edit_profile', 'edit_profile');
     });
 
-    Route::get('/desktop', [CaseIntakeController::class, 'show'])->name('intake.show');
+    Route::get('/desktop', [DesktopController::class, 'desktop']);
 });
 
 Route::group(['middleware' => 'guest'], function () {
