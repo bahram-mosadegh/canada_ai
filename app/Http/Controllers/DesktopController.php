6897573f901f5f70ede5cec168b7719d3abbe9d8
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-09-14 13:30:19 +0330

    Create DesktopController.php

---
+++ app/Http/Controllers/DesktopController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DesktopController extends Controller
+{
+    public function desktop()
+    {
+        return view('desktop');
+    }
+}
+
+
