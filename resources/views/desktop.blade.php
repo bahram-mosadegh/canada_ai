723c4bbd6f9dd5445ccec22bdca4d800bbd5477f
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-09-15 14:40:34 +0330

    Update desktop.blade.php

--- resources/views/desktop.blade.php
+++ resources/views/desktop.blade.php
@@ -1,12 +1,8 @@
 @extends('layouts.user_type.auth')
 
 @section('content')
-<div class="row justify-content-center mb-4">
-    <div class="col-md-8">
-        @livewire('case-intake')
-        @livewireScripts
-    </div>
-</div>
+    @livewire('case-intake')
+    @livewireScripts
 @endsection
 
 
