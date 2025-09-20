7d73238413f0ed761ac9ef222f87ff16570c082b
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-09-20 14:58:51 +0330

    Update ContractLookup.php

--- app/Services/ContractLookup.php
+++ app/Services/ContractLookup.php
@@ -2,40 +2,86 @@
 
 namespace App\Services;
 use App\Models\Client;
+use App\Models\ClientCase;
 use Illuminate\Support\Facades\Http;
 
 class ContractLookup
 {
+    private ?string $contract_number = null;
     private ?array $raw_data = null;
     private ?array $raw_case = null;
     private array $required_documents = [];
     private ?Client $client = null;
+    private ?ClientCase $case = null;
 
-    public function lookup(string $contractNumber)
+    public function lookup(string $contract_number)
     {
+        $this->contract_number = $contract_number;
+
         $domain = rtrim(env('CRM_ENDPOINT'), '/');
         $url = $domain.'/main/api/data/v9.0/new_evaluationforms';
         $username = env('CRM_USERNAME');
         $password = env('CRM_PASSWORD');
 
-        $response = Http::withOptions([
-            'auth' => [$username, $password, 'ntlm', 'domain' => $domain]
-        ])->get($url, [
-            '$select' => 'new_azmayeshgahdaran,new_bazneshastegan,new_dowlatcarmand,new_education,new_karfarmayansherkat,new_martial,new_mashaghelazad,new_mojeran,new_name,new_name1,new_name2,new_name3,new_name4,new_name5,new_name6,new_numberofpassengers,new_otherdocumenteducation,new_pezeshkdarayematab,new_pezeshkdarbimarestan,new_rezayatname2,new_sakhtemansazan,new_typeofvisa,new_typetahsili,new_vikalayedarayedaftar,new_vokalayeshagheldaftar',
-            '$filter' => "new_contractnumber2 eq '$contractNumber'",
-            '$expand' => 'new_contractnumber($select=new_mobilephone)'
-        ]);
+        // $response = Http::withOptions([
+        //     'auth' => [$username, $password, 'ntlm', 'domain' => $domain]
+        // ])->get($url, [
+        //     '$select' => 'new_azmayeshgahdaran,new_bazneshastegan,new_dowlatcarmand,new_education,new_karfarmayansherkat,new_martial,new_mashaghelazad,new_mojeran,new_name,new_name1,new_name2,new_name3,new_name4,new_name5,new_name6,new_numberofpassengers,new_otherdocumenteducation,new_pezeshkdarayematab,new_pezeshkdarbimarestan,new_rezayatname2,new_sakhtemansazan,new_typeofvisa,new_typetahsili,new_vikalayedarayedaftar,new_vokalayeshagheldaftar',
+        //     '$filter' => "new_contractnumber2 eq '$contract_number'",
+        //     '$expand' => 'new_contractnumber($select=new_mobilephone)'
+        // ]);
 
-        dd($domain, $url, $username, $password, $response->body());
+        // $this->raw_data = json_decode($response->body(), true);
 
-        $this->raw_data = json_decode($response->body(), true);
+        sleep(2);
+        $this->raw_data = json_decode('{
+    "@odata.context": "http://192.168.110.23/main/api/data/v9.0/$metadata#new_evaluationforms(new_azmayeshgahdaran,new_bazneshastegan,new_dowlatcarmand,new_education,new_karfarmayansherkat,new_martial,new_mashaghelazad,new_mojeran,new_name,new_name1,new_name2,new_name3,new_name4,new_name5,new_name6,new_numberofpassengers,new_otherdocumenteducation,new_pezeshkdarayematab,new_pezeshkdarbimarestan,new_rezayatname2,new_sakhtemansazan,new_typeofvisa,new_typetahsili,new_vikalayedarayedaftar,new_vokalayeshagheldaftar,new_contractnumber(new_mobilephone))",
+    "value": [
+        {
+            "@odata.etag": "W/\"37952131\"",
+            "new_azmayeshgahdaran": false,
+            "new_bazneshastegan": false,
+            "new_dowlatcarmand": false,
+            "new_education": false,
+            "new_karfarmayansherkat": false,
+            "new_martial": false,
+            "new_mashaghelazad": false,
+            "new_mojeran": false,
+            "new_name": "آیدا ربیعی",
+            "new_name1": "آیدا ربیعی",
+            "new_name2": null,
+            "new_name3": null,
+            "new_name4": null,
+            "new_name5": null,
+            "new_name6": null,
+            "new_numberofpassengers": 1,
+            "new_otherdocumenteducation": null,
+            "new_pezeshkdarayematab": true,
+            "new_pezeshkdarbimarestan": false,
+            "new_rezayatname2": false,
+            "new_sakhtemansazan": false,
+            "new_typeofvisa": 100000002,
+            "new_typetahsili": false,
+            "new_vikalayedarayedaftar": false,
+            "new_vokalayeshagheldaftar": false,
+            "new_evaluationformid": "dfef8507-1792-f011-9034-005056bfe138",
+            "new_contractnumber": {
+                "new_mobilephone": "09196610502"
+            }
+        }
+    ]
+}', true);
+
+        $this->client = null;
+        $this->case = null;
 
         return $this;
     }
 
-    public function make(array $raw_data)
+    public function make(array $raw_data, string $contract_number)
     {
         $this->raw_data = $raw_data;
+        $this->contract_number = $contract_number;
 
         return $this;
     }
@@ -44,7 +90,8 @@ class ContractLookup
     {
         return $this->raw_data
             && isset($this->raw_data['value'])
-            && is_array($this->raw_data['value'])
+            && isset($this->raw_data['value'][0])
+            && is_array($this->raw_data['value'][0])
             && count($this->raw_data['value']);
     }
 
@@ -58,7 +105,7 @@ class ContractLookup
         $this->raw_case = null;
 
         if ($this->isSuccessful()) {
-            $data = $this->raw_case['value'];
+            $data = $this->raw_data['value'][0];
 
             $applicant_names = [];
             if (isset($data['new_name1']) && $data['new_name1']) {
@@ -83,14 +130,17 @@ class ContractLookup
             $type_of_visa = 'unknown';
             if (isset($data['new_typeofvisa']) && $data['new_typeofvisa']) {
                 $type_of_visa = match ($data['new_typeofvisa']) {
-                    '100000000' => 'invitation',
-                    '100000001' => 'tourist_with_voucher_and_ticket',
-                    '100000002' => 'Tourist_with_company_invitation',
+                    100000000 => 'invitation',
+                    100000001 => 'tourist_with_voucher_and_ticket',
+                    100000002 => 'tourist_with_company_invitation',
                     default => 'unknown',
                 };
             }
 
-            $this->raw_case = [
+            $raw_case = [
+                'contract_number' => $this->contract_number,
+                'client_full_name' => $data['new_name'] ?? null,
+                'client_mobile' => $data['new_contractnumber']['new_mobilephone'] ?? null,
                 'number_of_applicants' => $data['new_numberofpassengers'] ?? 0,
                 'applicant_names' => $applicant_names,
                 'type_of_visa' => $type_of_visa,
@@ -107,6 +157,41 @@ class ContractLookup
                 'is_vokalayedarayedaftar' => $data['new_vikalayedarayedaftar'] ?? false,
                 'is_vokalayeshagheldaftar' => $data['new_vokalayeshagheldaftar'] ?? false,
             ];
+
+            $is_business = $raw_case['is_azmayeshgahdaran']
+                || $raw_case['is_karfarmayansherkat']
+                || $raw_case['is_mashaghelazad']
+                || $raw_case['is_mojeran']
+                || $raw_case['is_pezeshkdarayematab']
+                || $raw_case['is_sakhtemansazan']
+                || $raw_case['is_vokalayedarayedaftar'];
+
+            $raw_case['is_business'] = $is_business;
+
+            $required_documents = [
+                'id',
+                'travel_history',
+                'client',
+                'financial',
+                'purpose_of_travel',
+            ];
+
+            if ($is_business) {
+                $required_documents[] = 'business';
+            }
+
+            if ($type_of_visa == 'invitation') {
+                $required_documents = [
+                    'invitation',
+                    'proof_of_relationship'
+                ];
+            }
+
+            $raw_case['required_documents'] = $required_documents;
+
+            $this->required_documents = $required_documents;
+
+            $this->raw_case = $raw_case;
         }
 
         return $this->raw_case;
@@ -114,15 +199,8 @@ class ContractLookup
 
     public function getRequiredDocuments()
     {
-        $this->required_documents = [];
-
-        if ($this->isSuccessful()) {
-            $this->required_documents = [
-                'passport',
-                'bank_statement',
-                'resume',
-                'photo',
-            ];
+        if (!count($this->required_documents) && $this->isSuccessful()) {
+            $this->getRawCase();
         }
 
         return $this->required_documents;
@@ -135,14 +213,35 @@ class ContractLookup
 
     public function createClient()
     {
-        if ($this->raw_data) {
+        if ($this->isSuccessful() && !$this->client) {
+            $raw_case = $this->getRawCase();
+
             $this->client = Client::firstOrCreate(
-                ['email' => $this->raw_data['applicant']['email']],
-                ['full_name' => $this->raw_data['applicant']['full_name']]
+                ['mobile' => $raw_case['client_mobile']]
             );
         }
 
         return $this->client;
+    }
+
+    public function getCase()
+    {
+        return $this->case;
+    }
+
+    public function createCase()
+    {
+        if ($this->isSuccessful() && !$this->case) {
+            $raw_case = $this->getRawCase();
+
+            if ($this->client) {
+                $raw_case['client_id'] = $this->client->id;
+            }
+
+            $this->case = ClientCase::create($raw_case);
+        }
+
+        return $this->case;
     }
 }
 
