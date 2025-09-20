af057b3878402e3795817da9fec44150f7451cf8
bahram mosadegh <54316573+bahram-mosadegh@users.noreply.github.com>
2025-09-20 14:58:57 +0330

    Update 2025_09_07_070100_create_cases_table.php

--- database/migrations/2025_09_07_070100_create_cases_table.php
+++ database/migrations/2025_09_07_070100_create_cases_table.php
@@ -14,8 +14,26 @@ return new class extends Migration
         Schema::create('cases', function (Blueprint $table) {
             $table->id();
             $table->foreignId('client_id')->nullable();
-            $table->string('contract_number')->index();
-            $table->string('application_type')->nullable();
+            $table->string('contract_number')->nullable();
+            $table->string('client_full_name')->nullable();
+            $table->string('client_mobile')->nullable();
+            $table->integer('number_of_applicants')->default(0);
+            $table->json('applicant_names')->nullable();
+            $table->string('type_of_visa')->nullable();
+            $table->boolean('is_married')->default(false);
+            $table->boolean('is_azmayeshgahdaran')->default(false);
+            $table->boolean('is_bazneshastegan')->default(false);
+            $table->boolean('is_dowlatcarmand')->default(false);
+            $table->boolean('is_karfarmayansherkat')->default(false);
+            $table->boolean('is_mashaghelazad')->default(false);
+            $table->boolean('is_mojeran')->default(false);
+            $table->boolean('is_pezeshkdarayematab')->default(false);
+            $table->boolean('is_pezeshkdarbimarestan')->default(false);
+            $table->boolean('is_sakhtemansazan')->default(false);
+            $table->boolean('is_vokalayedarayedaftar')->default(false);
+            $table->boolean('is_vokalayeshagheldaftar')->default(false);
+            $table->boolean('is_business')->default(false);
+            $table->json('required_documents')->nullable();
             $table->string('status')->default('in_progress');
             $table->timestamps();
         });
